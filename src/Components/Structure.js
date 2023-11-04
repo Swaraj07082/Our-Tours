ad744601452c920abfb91a3673acc44d692de563
Swaraj <swarajmali072004@gmail.com>
2023-11-05 04:38:47 +0530

    Initial Commit

---
+++ src/Components/Structure.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function Structure() {
+  return (
+    <>
+    
+    <div className="container text-center"  >
+  <div className="row">
+    <div className="col">
+      <img src="./images/ireland.jpg" alt=""   style={{    width: 352,
+    height: 320}}/>
+    </div>
+    <div className="col">
+
+      <img src="./images/rome.jpg" alt=""  style={{    width: 352,
+    height: 320}} />
+    </div>
+    <div className="col">
+    <img src="./images/paris.jpg" alt=""  style={{    width: 352,
+    height: 320}}/>
+    </div>
+  </div>
+</div>
+
+
+
+
+    
+    </>
+  )
+}
